e26edc5534897c0941721ee25251954352f776e0
jabontz09 <jabontz09@gmail.com>
2023-02-16 22:24:32 -0500

    Add project files.

---
+++ BlazorEcommerceTut/Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BlazorEcommerceTut.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerceTut.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ProductController(DataContext context)
+        {
+            this.context = context;
+        }
+   
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await context.Products.ToListAsync());
+        }
+    }
+}
